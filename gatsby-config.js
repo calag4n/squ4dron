d9bedcac3ab220936a5d176e66dd56217cc79251
calag4n <dieu.st@gmail.com>
2019-03-25 22:06:06 +0100

    add web manifest

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Antre Cool`,
+        short_name: `AntreCool`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#9060eb`,
+        display: `standalone`,
+        icon: `src/ec.png`, // This path is relative to the root of the site.
+        include_favicon: true, // Include favicon
+      },
+    },
+  ],
+}
